74477090de801e18f92eaf6a34577b0bda7f472c
Emirhan Gözüküçük <emirhan_g7@outlook.com>
2024-01-07 19:33:35 +0300

    v1.0

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react'
+import MovieCard from './MovieCard';
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
+import HrMovieCard from './HrMovieCard';
+function MovieList({genreId,index_}) {
+    const [movieList,setMovieList]=useState([])
+    const elementRef=useRef(null);
+    useEffect(()=>{
+        getMovieByGenreId();
+    },[])
+
+    const getMovieByGenreId=()=>{
+
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf&with_genres=${genreId}`)
+        .then(resp=>resp.json())
+        .then(data=>{
+            setMovieList(data.results)
+        })
+
+    }
+
+    const slideRight=(element)=>{
+        element.scrollLeft+=500;
+    }
+    const slideLeft=(element)=>{
+        element.scrollLeft-=500;
+    }
+  return (
+    <div className='relative'>
+         <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
+         className={`text-[50px] text-white
+           p-2 z-10 cursor-pointer 
+            hidden md:block absolute
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
+   
+    <div ref={elementRef} className='flex overflow-x-auto gap-8
+     scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
+        {movieList.map((item,index)=>(
+           <>
+          {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
+           </> 
+        ))}
+    </div>
+    <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
+           className={`text-[50px] text-white hidden md:block
+           p-2 cursor-pointer z-10 top-0
+            absolute right-0 
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/>
+    </div>
+  )
+}
+
+export default MovieList
